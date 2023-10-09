95261e807dba86b1e95e6a8a40e6fb1b725db04a
KNAthari <143450459+KNAthari@users.noreply.github.com>
2023-10-09 15:47:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("test");
+$(document).ready(function(){
+    $("#minimize").on("click", function(){
+        $(".footnote").css("font-size", "10px");
+        $(".footer").css("font-size", "7px");
+    })
+});
+       
+        
